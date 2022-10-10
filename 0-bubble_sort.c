425402d21c33f9a7d4a81769aa4d0bfcb026b3c4
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-10-10 20:10:49 +0000

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,18 +13,20 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp = 0;
-	int *array = malloc(size * sizeof(int));
+	int *arrayB = malloc(size * sizeof(int));
+
+	arrayB = array;
 
 	for (i = 1; i < size; i++)
 	{
 		for (j = 0; j < size - 1; j++)
 		{
-			if (array[j] > array[j + 1])
+			if (arrayB[j] > arrayB[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
+				temp = arrayB[j];
+				arrayB[j] = arrayB[j + 1];
+				arrayB[j + 1] = temp;
+				print_array(arrayB, size);
 			}
 		}
 	}
