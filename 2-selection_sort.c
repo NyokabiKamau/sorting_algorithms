97fd6f14bd18b0cba6dccc29b8c7a542ac9f2af4
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-10-11 10:33:49 +0000

    update

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort - A function that sorts an array of integers in ascending order
+ * using selection sort algorithm.
+ * You’re expected to print the array after each time you swap two elements 
+ * @array: An array to sort.
+ * @size: The size of the array.
+ */
+void selection_sort(int *array, size_t size)
+{
+	int temp = 0;
+	size_t i = 0, j = 0, min = 0;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -1,7 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -23,11 +22,9 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
-int len_list(listint_t *h);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_recursion(int *array, int left, int right, size_t size);
 int partition(int *array, int left, int right, size_t size);
 void shell_sort(int *array, size_t size);
 void switch_nodes(listint_t **list, listint_t **p);
